c308b898639d1404f60a6264926cc459714be296
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-06-14 14:26:49 +0500

    Update routes.py

--- shop-order-api/product-service/app/routes.py
+++ shop-order-api/product-service/app/routes.py
@@ -1,8 +1,9 @@
-import httpx
-from fastapi import Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from app.config import settings
+import httpx
 
+router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
